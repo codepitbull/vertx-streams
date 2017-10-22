43041e1366c8fcc738123d869c61d1ca3c9f73ba
Jochen Mader <pflanzenmoerder@gmail.com>
2017-10-22 23:02:38 +0200

    Removed unused code

--- src/test/scala/io/vertx/lang/scala/streams/reactivestreams/WriteStreamSubscriberTCKTest.scala
+++ src/test/scala/io/vertx/lang/scala/streams/reactivestreams/WriteStreamSubscriberTCKTest.scala
@@ -10,24 +10,6 @@ class WriteStreamSubscriberTCKTest(testEnvironment: TestEnvironment, gcTimeOut: 
 
   def this() = this(new TestEnvironment(300l), 1000L)
 
-
-
-//  override def createPublisher(amount: Long): Publisher[String] = {
-//    val vertx = Vertx.vertx()
-//    val ctx = vertx.getOrCreateContext()
-//    implicit val ec = new VertxExecutionContext(ctx)
-//    val listSource =  if(amount < 300)
-//                        new VertxListSource[String]((1 to amount.toInt).toList.map(i => i.toString))
-//                      else
-//                        new VertxListSource[String](List())
-//    listSource.stream.publisher()
-//  }
-//
-//  override def createFailedPublisher(): Publisher[String] = {
-//    null
-//  }
-
-
   override def createSubscriber() = {
     val vertx = Vertx.vertx()
     val ctx = vertx.getOrCreateContext()
